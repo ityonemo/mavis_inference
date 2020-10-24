c8a908d6a6bf5275733b088837549be5befa2879
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2020-10-23 21:15:59 -0700

    refactored module tests, then create a failing test that isolates the too many blocks problem

--- test/module_test.exs
+++ test/module_test.exs
@@ -8,32 +8,49 @@ defmodule TypeTest.ModuleTest do
 
   import Type
 
-  describe "when the module has an exported function via def" do
-    test "from_binary/1 produces an entry point for an exported function" do
-      {_, binary, _} = :code.get_object_code(TypeTest.ModuleExamples.WithDef)
+  defp common_setup(submodule) do
+    {_, binary, _} = TypeTest.ModuleExamples
+    |> Elixir.Module.concat(submodule)
+    |> :code.get_object_code
 
-      {:ok, module} = Module.from_binary(binary)
+    {:ok, module_struct} = Module.from_binary(binary)
+    {:ok, module: module_struct}
+  end
 
-      assert %Module{entry_points: %{{:function, 1} => _ep}} = module
+  describe "when the module has an exported function via def, from_binary/1" do
+
+    setup do
+      common_setup(WithDef)
+    end
+
+    test "produces an entry point for an exported function" , %{module: module} do
+      assert %Module{entry_points: %{{:function, 1} => ep}} = module
+
+      assert [block] = module.label_blocks[ep]
+
+      assert %{0 => builtin(:any)} = block.makes.xreg
+      assert %{0 => builtin(:any)} = block.needs.xreg
     end
   end
 
-  describe "when the module has a private function via defp" do
-    test "from_binary/1 produces an entry point for the private function" do
-      {_, binary, _} = :code.get_object_code(TypeTest.ModuleExamples.WithDefp)
+  describe "when the module has a private function via defp, from_binary/1" do
 
-      {:ok, module} = Module.from_binary(binary)
+    setup do
+      common_setup(WithDefp)
+    end
 
+    test "produces an entry point for the private function", %{module: module} do
       assert %Module{entry_points: %{{:functionp, 1} => _ep}} = module
     end
   end
 
-  describe "when the module has a lambda" do
-    test "from_binary/1 produces an entry point for the lambda" do
-      {_, binary, _} = :code.get_object_code(TypeTest.ModuleExamples.WithLambda)
+  describe "when the module has a lambda, from_binary/1" do
 
-      {:ok, module} = Module.from_binary(binary)
+    setup do
+      common_setup(WithLambda)
+    end
 
+    test "produces an entry point for the lambda", %{module: module} do
       assert %Module{entry_points: %{{:"-fun.functionp/1-", 1} => _ep}} = module
     end
   end
