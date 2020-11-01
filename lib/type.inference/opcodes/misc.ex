e4db6e7127703b82be04bd7066bd0eab1d2dd9af
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2020-10-31 23:22:12 -0700

    implementation of the basic jump opcode

--- lib/type.inference/opcodes/misc.ex
+++ lib/type.inference/opcodes/misc.ex
@@ -2,6 +2,8 @@ defmodule Type.Inference.Opcodes.Misc do
 
   use Type.Inference.Macros
 
+  alias Type.Inference.Module.ParallelParser
+
   opcode {:line, _}, :noop
 
   opcode {:allocate, _, _}, :noop
@@ -11,4 +13,18 @@ defmodule Type.Inference.Opcodes.Misc do
   opcode {:init, _}, :noop
 
   opcode {:test_heap, _, _}, :noop
+
+  opcode {:jump, {:f, dest}} do
+    forward(state, _meta, ...) do
+      [jump_blk] = ParallelParser.obtain_label(dest)
+
+      if reg = Enum.find(Map.keys(jump_blk.needs), &(!is_map_key(state.x, &1))) do
+        {:backprop, [put_reg(state, reg, jump_blk.needs[reg])]}
+      else
+        {:ok, put_reg(state, 0, jump_blk.makes)}
+      end
+    end
+
+    backprop :terminal
+  end
 end
