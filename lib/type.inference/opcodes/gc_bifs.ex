58b3c84618430658ee0ebb46d6e2216510134089
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2020-11-06 22:31:48 -0800

    finished refactoring gc_bifs

--- lib/type.inference/opcodes/gc_bifs.ex
+++ lib/type.inference/opcodes/gc_bifs.ex
@@ -2,21 +2,21 @@ defmodule Type.Inference.Opcodes.GcBifs do
   use Type.Inference.Opcodes
 
   opcode {:gc_bif, :bit_size, _, _, [from], to} do
-    forward(state, _meta, ...) do
-      if is_defined(state, from) do
-        {:ok, put_reg(state, to, builtin(:non_neg_integer))}
-      else
-        prev_state = state
-        |> tombstone(to)
-        |> put_reg(from, %Type.Bitstring{size: 0, unit: 1})
+    forward(regs, _meta, ...) when not is_defined(regs, from) do
+      prev_state = regs
+      |> tombstone(to)
+      |> put_reg(from, %Type.Bitstring{size: 0, unit: 1})
 
-        {:backprop, [prev_state]}
-      end
+      {:backprop, [prev_state]}
     end
 
-    backprop(state, _meta, ...) do
-      if Type.subtype?(fetch_type(state, to), builtin(:non_neg_integer)) do
-        {:ok, [put_reg(state, from, %Type.Bitstring{size: 0, unit: 1})]}
+    forward(regs, _meta, ...) do
+      {:ok, put_reg(regs, to, builtin(:non_neg_integer))}
+    end
+
+    backprop(regs, _meta, ...) do
+      if Type.subtype?(fetch_type(regs, to), builtin(:non_neg_integer)) do
+        {:ok, [put_reg(regs, from, %Type.Bitstring{size: 0, unit: 1})]}
       else
         {:ok, []}
       end
@@ -24,21 +24,21 @@ defmodule Type.Inference.Opcodes.GcBifs do
   end
 
   opcode {:gc_bif, :byte_size, _, _, [from], to} do
-    forward(state, _meta, ...) do
-      if is_defined(state, from) do
-        {:ok, put_reg(state, to, builtin(:non_neg_integer))}
-      else
-        prev_state = state
-        |> tombstone(to)
-        |> put_reg(from, %Type.Bitstring{size: 0, unit: 8})
+    forward(regs, _meta, ...) when not is_defined(regs, from) do
+      prev_state = regs
+      |> tombstone(to)
+      |> put_reg(from, %Type.Bitstring{size: 0, unit: 8})
 
-        {:backprop, [prev_state]}
-      end
+      {:backprop, [prev_state]}
     end
 
-    backprop(state, _meta, ...) do
-      if Type.subtype?(fetch_type(state, to), builtin(:non_neg_integer)) do
-        {:ok, [put_reg(state, from, %Type.Bitstring{size: 0, unit: 1})]}
+    forward(regs, _meta, ...) do
+      {:ok, put_reg(regs, to, builtin(:non_neg_integer))}
+    end
+
+    backprop(regs, _meta, ...) do
+      if Type.subtype?(fetch_type(regs, to), builtin(:non_neg_integer)) do
+        {:ok, [put_reg(regs, from, %Type.Bitstring{size: 0, unit: 1})]}
       else
         {:ok, []}
       end
@@ -46,15 +46,20 @@ defmodule Type.Inference.Opcodes.GcBifs do
   end
 
   opcode {:gc_bif, :length, _fail, _, [from], to} do
-    forward(state, _meta, ...) do
+    forward(regs, _meta, ...) when not is_defined(regs, from) do
+      {:backprop, [put_reg(regs, from, %Type.List{})]}
+    end
+
+    forward(regs, _meta, ...) when is_reg(regs, from, []) do
+      {:ok, put_reg(regs, to, 0)}
+    end
+
+    forward(regs, _meta, ...) do
       cond do
-        not is_defined(state, from) ->
-          {:backprop, [put_reg(state, from, %Type.List{})]}
-        fetch_type(state, from) == [] -> {:ok, put_reg(state, to, 0)}
-        match?(%Type.List{nonempty: true}, fetch_type(state, from)) ->
-          {:ok, put_reg(state, to, builtin(:pos_integer))}
-        match?(%Type.List{}, fetch_type(state, from)) ->
-          {:ok, put_reg(state, to, builtin(:non_neg_integer))}
+        match?(%Type.List{nonempty: true}, fetch_type(regs, from)) ->
+          {:ok, put_reg(regs, to, builtin(:pos_integer))}
+        match?(%Type.List{}, fetch_type(regs, from)) ->
+          {:ok, put_reg(regs, to, builtin(:non_neg_integer))}
       end
     end
 
@@ -62,16 +67,16 @@ defmodule Type.Inference.Opcodes.GcBifs do
   end
 
   opcode {:gc_bif, :map_size, _fail, _, [from], to} do
-    forward(state, _meta, ...) do
-      if is_defined(state, from) do
-        {:ok, put_reg(state, to, builtin(:non_neg_integer))}
-      else
-        prev_state = state
-        |> tombstone(to)
-        |> put_reg(from, %Type.Map{optional: %{builtin(:any) => builtin(:any)}})
+    forward(regs, _meta, ...) when not is_defined(regs, from) do
+      prev_state = regs
+      |> tombstone(to)
+      |> put_reg(from, %Type.Map{optional: %{builtin(:any) => builtin(:any)}})
 
-        {:backprop, [prev_state]}
-      end
+      {:backprop, [prev_state]}
+    end
+
+    forward(regs, _meta, ...) do
+      {:ok, put_reg(regs, to, builtin(:non_neg_integer))}
     end
 
     backprop :terminal
@@ -101,22 +106,22 @@ defmodule Type.Inference.Opcodes.GcBifs do
   defp do_add(builtin(:integer), t) when t in @all_int_types,        do: builtin(:integer)
 
   opcode {:gc_bif, :+, _, _, [left, right], to} do
-    forward(state, _meta, ...) do
+    foward(regs, _meta, ...) do
       cond do
         # TODO: make this a guard.
-        not is_defined(state, left) ->
-          {:backprop, Enum.map(@int_types ++ @num_types, &put_reg(state, left, &1))}
-        not is_defined(state, right) and fetch_type(state, left) in @int_types ->
-          {:backprop, Enum.map(@int_types, &put_reg(state, right, &1))}
-        not is_defined(state, right) and fetch_type(state, left) == builtin(:float) ->
-          {:backprop, [put_reg(state, right, builtin(:float)), put_reg(state, right, builtin(:integer))]}
-        not is_defined(state, right) and fetch_type(state, left) == builtin(:integer) ->
-          {:backprop, [put_reg(state, right, builtin(:float))]}
+        not is_defined(regs, left) ->
+          {:backprop, Enum.map(@int_types ++ @num_types, &put_reg(regs, left, &1))}
+        not is_defined(regs, right) and fetch_type(regs, left) in @int_types ->
+          {:backprop, Enum.map(@int_types, &put_reg(regs, right, &1))}
+        not is_defined(regs, right) and fetch_type(regs, left) == builtin(:float) ->
+          {:backprop, [put_reg(regs, right, builtin(:float)), put_reg(regs, right, builtin(:integer))]}
+        not is_defined(regs, right) and fetch_type(regs, left) == builtin(:integer) ->
+          {:backprop, [put_reg(regs, right, builtin(:float))]}
         true ->
-          ltype = fetch_type(state, left)
-          rtype = fetch_type(state, right)
+          ltype = fetch_type(regs, left)
+          rtype = fetch_type(regs, right)
           res = do_add(ltype, rtype)
-          {:ok, put_reg(state, to, res)}
+          {:ok, put_reg(regs, to, res)}
       end
     end
 
@@ -144,22 +149,22 @@ defmodule Type.Inference.Opcodes.GcBifs do
   defp do_sub(builtin(:integer), t) when t in @all_int_types,        do: builtin(:integer)
 
   opcode {:gc_bif, :-, _, _, [left, right], to} do
-    forward(state, _meta, ...) do
+    forward(regs, _meta, ...) do
       cond do
         # TODO: make this a guard.
-        not is_defined(state, left) ->
-          {:backprop, Enum.map(@int_types ++ @num_types, &put_reg(state, left, &1))}
-        not is_defined(state, right) and fetch_type(state, left) in @int_types ->
-          {:backprop, Enum.map(@int_types, &put_reg(state, right, &1))}
-        not is_defined(state, right) and fetch_type(state, left) == builtin(:float) ->
-          {:backprop, [put_reg(state, right, builtin(:float)), put_reg(state, right, builtin(:integer))]}
-        not is_defined(state, right) and fetch_type(state, left) == builtin(:integer) ->
-          {:backprop, [put_reg(state, right, builtin(:float))]}
+        not is_defined(regs, left) ->
+          {:backprop, Enum.map(@int_types ++ @num_types, &put_reg(regs, left, &1))}
+        not is_defined(regs, right) and fetch_type(regs, left) in @int_types ->
+          {:backprop, Enum.map(@int_types, &put_reg(regs, right, &1))}
+        not is_defined(regs, right) and fetch_type(regs, left) == builtin(:float) ->
+          {:backprop, [put_reg(regs, right, builtin(:float)), put_reg(regs, right, builtin(:integer))]}
+        not is_defined(regs, right) and fetch_type(regs, left) == builtin(:integer) ->
+          {:backprop, [put_reg(regs, right, builtin(:float))]}
         true ->
-          ltype = fetch_type(state, left)
-          rtype = fetch_type(state, right)
+          ltype = fetch_type(regs, left)
+          rtype = fetch_type(regs, right)
           res = do_sub(ltype, rtype)
-          {:ok, put_reg(state, to, res)}
+          {:ok, put_reg(regs, to, res)}
       end
     end
 
@@ -186,22 +191,22 @@ defmodule Type.Inference.Opcodes.GcBifs do
   defp do_mul(builtin(:integer), t) when t in @all_int_types,        do: builtin(:integer)
 
   opcode {:gc_bif, :*, _, _, [left, right], to} do
-    forward(state, _meta, ...) do
+    forward(regs, _meta, ...) do
       cond do
         # TODO: make this a guard.
-        not is_defined(state, left) ->
-          {:backprop, Enum.map(@int_types ++ @num_types, &put_reg(state, left, &1))}
-        not is_defined(state, right) and fetch_type(state, left) in @int_types ->
-          {:backprop, Enum.map(@int_types, &put_reg(state, right, &1))}
-        not is_defined(state, right) and fetch_type(state, left) == builtin(:float) ->
-          {:backprop, [put_reg(state, right, builtin(:float)), put_reg(state, right, builtin(:integer))]}
-        not is_defined(state, right) and fetch_type(state, left) == builtin(:integer) ->
-          {:backprop, [put_reg(state, right, builtin(:float))]}
+        not is_defined(regs, left) ->
+          {:backprop, Enum.map(@int_types ++ @num_types, &put_reg(regs, left, &1))}
+        not is_defined(regs, right) and fetch_type(regs, left) in @int_types ->
+          {:backprop, Enum.map(@int_types, &put_reg(regs, right, &1))}
+        not is_defined(regs, right) and fetch_type(regs, left) == builtin(:float) ->
+          {:backprop, [put_reg(regs, right, builtin(:float)), put_reg(regs, right, builtin(:integer))]}
+        not is_defined(regs, right) and fetch_type(regs, left) == builtin(:integer) ->
+          {:backprop, [put_reg(regs, right, builtin(:float))]}
         true ->
-          ltype = fetch_type(state, left)
-          rtype = fetch_type(state, right)
+          ltype = fetch_type(regs, left)
+          rtype = fetch_type(regs, right)
           res = do_mul(ltype, rtype)
-          {:ok, put_reg(state, to, res)}
+          {:ok, put_reg(regs, to, res)}
       end
     end
 
