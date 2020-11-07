91392770cbf181e47e3952747b33fe4d10da4a5e
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2020-11-06 22:44:59 -0800

    refactor gets

--- lib/type.inference/opcodes/gets.ex
+++ lib/type.inference/opcodes/gets.ex
@@ -17,34 +17,32 @@ defmodule Type.Inference.Opcodes.Gets do
   end
 
   opcode {:get_list, from, head, tail} do
+    forward(state, _meta, ...) when not is_defined(state, from) do
+      {:backprop, [put_reg(state, from, %Type.List{nonempty: true, final: builtin(:any)})]}
+    do
+
     forward(state, _meta, ...) do
-      cond do
-        not is_defined(state, from) ->
-          {:backprop, [put_reg(state, from, %Type.List{nonempty: true, final: builtin(:any)})]}
-        true ->
-          from_type = fetch_type(state, from)
+      from_type = fetch_type(state, from)
 
-          new_state = state
-          |> put_reg(head, from_type.type)
-          |> put_reg(tail, Type.union(from_type, from_type.final))
+      new_state = state
+      |> put_reg(head, from_type.type)
+      |> put_reg(tail, Type.union(from_type, from_type.final))
 
-          {:ok, new_state}
-      end
+      {:ok, new_state}
     end
 
     backprop :terminal
   end
 
   opcode {:get_tl, from, to} do
-    forward(state, _meta, ...) do
-      cond do
-        not is_defined(state, from) ->
-          {:backprop, [put_reg(state, from, %Type.List{nonempty: true, final: builtin(:any)})]}
-        true ->
-          from_type = fetch_type(state, from)
+    forward(state, _meta, ...) when not is_defined(state, from) do
+      {:backprop, [put_reg(state, from, %Type.List{nonempty: true, final: builtin(:any)})]}
+    do
 
-          {:ok, put_reg(state, to, Type.union(from_type, from_type.final))}
-      end
+    forward(state, _meta, ...) do
+      from_type = fetch_type(state, from)
+
+      {:ok, put_reg(state, to, Type.union(from_type, from_type.final))}
     end
 
     backprop :terminal
